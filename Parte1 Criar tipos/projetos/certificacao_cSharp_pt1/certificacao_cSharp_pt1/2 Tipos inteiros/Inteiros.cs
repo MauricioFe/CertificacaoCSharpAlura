06fb107b8271ef73b3a113c727f3bbfcd305dc63
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-01-05 16:22:36 -0300

    Trabalhando tipos inteiros

---
+++ Parte1 Criar tipos/projetos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/2 Tipos inteiros/Inteiros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_cSharp_pt1._2_Tipos_inteiros
+{
+    class Inteiros : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 15;
+        }
+    }
+}
