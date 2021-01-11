0569f80f1a418aec5ee7c56b9949132253ed3807
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-01-11 10:45:29 -0300

    trabalhando todos os tipos inteiros do C#

---
+++ Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/2 Tipos inteiros/Inteiros.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_cSharp_pt1._2_Tipos_inteiros
+{
+    class Inteiros : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 15;
+            //idade = 15.5;
+
+            char resposta = 'S'; //System.char
+
+            byte nivelDeAzul = 0xFF; //255 em hexadecimal
+            short passageirosVoo = 230;//System.Int16
+            passageirosVoo = -6;
+
+            int populacao = 1500; //System.Int32
+            populacao = -2300;
+
+            long populacaoDoBrasil = 207_660_929; //System.Int64 +/-207 milhões
+
+            sbyte nivelDeBrilho = -127;
+
+            ushort passageirosNavio = 2300;
+            uint estoque = 2300000;
+            ulong populacaoDoMundo = 7_000_000_000;
+
+            Console.WriteLine($"Resposta: {resposta}");
+
+            Console.WriteLine($"Nivel de Azul: {nivelDeAzul}");
+            Console.WriteLine($"passageirosVoo: {passageirosVoo}");
+            Console.WriteLine($"populacao: {populacao}");
+            Console.WriteLine($"populacaoDoBrasil: {populacaoDoBrasil}");
+
+            Console.WriteLine($"nivelDeBrilho: {nivelDeBrilho}");
+            Console.WriteLine($"passageirosNavio: {passageirosNavio}");
+            Console.WriteLine($"estoque: {estoque}");
+            Console.WriteLine($"populacaoDoMundo: {populacaoDoMundo}");
+
+        }
+    }
+}
