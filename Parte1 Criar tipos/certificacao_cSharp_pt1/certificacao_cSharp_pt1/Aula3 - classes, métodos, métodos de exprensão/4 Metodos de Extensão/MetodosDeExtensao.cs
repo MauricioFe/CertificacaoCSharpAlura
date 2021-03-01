5bd6c07701f3ca074b78f6bec83284a593b538f4
Maurício Lacerda <mauricio.laceradaml@gmail.com>
2021-03-01 10:39:55 -0300

    Trabalhando com métodos de extensão

---
+++ Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Aula3 - classes, métodos, métodos de exprensão/4 Metodos de Extensão/MetodosDeExtensao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_cSharp_pt1.Aula3___classes__métodos__métodos_de_exprensão._4_Metodos_de_Extensão
+{
+    class MetodosDeExtensao : IAulaItem
+    {
+        public void Executar()
+        {
+            Impressora impressora = new Impressora("Esse é \r\no meu documento");
+            impressora.ImprimirDocumento();
+            ImprimirDocumentoHTML(impressora.Documento);
+            impressora.ImprimirDocumentoHTML();
+        }
+        void ImprimirDocumentoHTML(string documento)
+        {
+            Console.WriteLine($"<html><body>{documento}</body></html>");
+        }
+
+
+    }
+    class Impressora
+    {
+        public Impressora(string documento)
+        {
+            this.Documento = documento;
+        }
+
+        public string Documento { get; }
+
+        internal void ImprimirDocumento()
+        {
+            Console.WriteLine();
+            Console.WriteLine(Documento);
+        }
+       
+    }
+    static class ImpressoraExtensions
+    {
+        // a palavra this indica que eu estou extendendo a classe impressora com o método abaixo
+        public static void ImprimirDocumentoHTML(this Impressora impressora)
+        {
+            Console.WriteLine($"<html><body>{impressora.Documento}</body></html>");
+        }
+    }
+}
--- Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Program.cs
+++ Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Program.cs
@@ -12,6 +12,7 @@ using certificacao_cSharp_pt1.Aula2___Criar_Tipos_de_referência._6_Dynamic;
 using certificacao_cSharp_pt1.Aula2___Criar_Tipos_de_referência._7_Strings;
 using certificacao_cSharp_pt1.Aula3___classes__métodos__métodos_de_exprensão._2_Metodos;
 using certificacao_cSharp_pt1.Aula3___classes__métodos__métodos_de_exprensão._3_Parametros;
+using certificacao_cSharp_pt1.Aula3___classes__métodos__métodos_de_exprensão._4_Metodos_de_Extensão;
 using certificacao_cSharp_pt1.Tipos_de_Valor;
 using System;
 using System.Collections.Generic;
@@ -102,7 +103,7 @@ namespace certificacao_cSharp_pt1
                 new MenuItem("Metodos", typeof(Metodos)),
                 new MenuItem("Parâmetros Nomeados", typeof(ParametrosNomeados)),
                 new MenuItem("Parâmetros Opcionais", typeof(ParametrosOpcionais)),
-                //new MenuItem("Métodos de Extensão", typeof(MetodosDeExtensao)),
+                new MenuItem("Métodos de Extensão", typeof(MetodosDeExtensao)),
                 //new MenuItem("Propriedades Indexadas", typeof(PropriedadesIndexadas)),
                 //new MenuItem("Sobrecargas", typeof(Sobrecargas)),
                 //new MenuItem("Métodos Substituidos", typeof(MetodosSubstituidos)),
