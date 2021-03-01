8fae3e127b6a9867a805ea979aad4299aa6c13db
Maurício Lacerda <mauricio.laceradaml@gmail.com>
2021-03-01 09:15:10 -0300

    Trabalhando com a classe String

--- Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Aula2 - Criar Tipos de referência/7 Strings/Strings.cs
+++ Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Aula2 - Criar Tipos de referência/7 Strings/Strings.cs
@@ -10,7 +10,19 @@ namespace certificacao_cSharp_pt1.Aula2___Criar_Tipos_de_referência._7_Strings
     {
         public void Executar()
         {
-            throw new NotImplementedException();
+            string a = "bom dia";
+            string b = "b";
+
+
+            b += "om dia";
+            Console.WriteLine($"a == b: {a == b}");
+            Console.WriteLine($"(object)a == (object)b: {(object)a == (object)b}");
+            string bomDia = "bom dia";
+
+            Console.WriteLine($"bomDia[4]: {bomDia[4]}");
+
+            var caractere = bomDia[4];
+            Console.WriteLine($"caractere.GetType(): {caractere.GetType()}");
         }
     }
 }
