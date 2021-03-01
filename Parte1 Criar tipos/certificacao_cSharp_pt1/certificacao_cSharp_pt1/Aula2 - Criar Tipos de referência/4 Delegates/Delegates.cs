a2b9ab2c8c5ca1a9ac0f8263345350c82328d1be
Maurício Lacerda <mauricio.laceradaml@gmail.com>
2021-03-01 08:51:44 -0300

    Trabalhando com delegates

--- Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Aula2 - Criar Tipos de referência/4 Delegates/Delegates.cs
+++ Parte1 Criar tipos/certificacao_cSharp_pt1/certificacao_cSharp_pt1/Aula2 - Criar Tipos de referência/4 Delegates/Delegates.cs
@@ -10,7 +10,45 @@ namespace certificacao_cSharp_pt1.Aula2___Criar_Tipos_de_referência._4_Delegate
     {
         public void Executar()
         {
-            throw new NotImplementedException();
+            Calculadora.Executar();
+        }
+    }
+
+    delegate double MetodoMultiplicacao(double input);
+
+    class Calculadora
+    {
+        static double Duplicar(double input)
+        {
+            return input * 2;
+        }
+        static double Triplicar(double input)
+        {
+            return input * 3;
+        }
+        public static void Executar()
+        {
+            Console.WriteLine($"Duplicar (7.5): {Duplicar(7.5)}");
+            Console.WriteLine($"Triplicar (7.5): {Triplicar(7.5)}");
+
+            MetodoMultiplicacao metodoMultiplicacao = Duplicar;
+            Console.WriteLine($"Delegate duplicar {metodoMultiplicacao(7.5)}");
+            metodoMultiplicacao = Triplicar;
+            Console.WriteLine($"Delegate triplicar {metodoMultiplicacao(7.5)}");
+            //delegate com implementação anônima
+            MetodoMultiplicacao metodoQuadrado = delegate (double input)
+            {
+                return input * input;
+            };
+
+            double quadrado = metodoQuadrado(5);
+            Console.WriteLine($"quadrado: {quadrado}");
+
+            //expressão lambda
+            MetodoMultiplicacao metodoCubo = input => input * input * input;
+
+            double cubo = metodoQuadrado(4.375);
+            Console.WriteLine($"cubo {cubo}");
         }
     }
 }
